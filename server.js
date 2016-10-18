08243f329120e41cc812a29cc3109f8b3ab365cf
Mrigank11 <mrigankkrishan@gmail.com>
2016-10-18 18:08:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+//server.js
+var http = require('http');
+handler=function(req,res){
+    makeHTTPRequest(req,res);
+}
+
+function makeHTTPRequest(request,response) {
+    var proxy=http.request(request.headers.host);
+
+    response.writeHead(200);
+    response.end('HI');
+}
+
+http.createServer(handler).listen(8080);
