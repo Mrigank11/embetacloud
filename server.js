32a66be4cacc8ec1a3107b4ebb778d0e8cfb4878
Mrigank11 <mrigankkrishan@gmail.com>
2016-10-18 18:39:04 +0530

    fix

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ handler=function(req,res){
 }
 
 function makeHTTPRequest(request,response) {
-    var proxy=http.request(request.headers.host);
-
     response.writeHead(200);
     response.end('HI');
 }
