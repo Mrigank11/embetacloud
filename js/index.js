8580e9a273530fa617dd317d9ae9d4f5785ac818
Mrigank11 <mrigankkrishan@gmail.com>
2016-10-23 14:44:23 +0530

    semantic-ui fixes

--- js/index.js
+++ js/index.js
@@ -1,16 +1,71 @@
 var socket = io.connect();
-
 var app = angular.module("app", []);
-app.controller("main", function ($scope,$timeout) {
-    socket.emit('get-consent-page-url');
-    socket.on('consent-page-url', function (data) {
-        $timeout(function(){$scope.consentPageUrl = data.url;});
+app.controller("main", function ($scope, $timeout) {
+    //Init
+    $scope.visitedPages = {};
+    //socket emits
+    socket.on('status', function (data) {
+        $timeout(function () {
+            $scope.status = {};
+            $scope.status.consentPageUrl = data.url;
+            $scope.status.logged = data.logged;
+        });
     });
-    $scope.visitedPages=[];
-    socket.on('pageVisited',function(data){
-        $timeout(function(){$scope.visitedPages.push(data.url)});
+    socket.on('pageVisited', function (data) {
+        $timeout(function () {
+            $scope.visitedPages[data.id] = data;
+        });
     });
-    $scope.clearVisitedPages=function(){
-        $scope.visitedPages=[];
+    socket.on('progress', function (data) {
+        $scope.setProgress(data);
+    });
+    socket.on('driveUploadSuccess', function (data) {
+        var id = data.id;
+        var name = data.name;
+        $timeout(function () {
+            $scope.visitedPages[id].msg = "Uploaded " + name + " to Drive";
+        });
+    });
+    //Functions
+    $scope.downloadToPC = function (page) {
+        window.location.href = page.path;
+    }
+    $scope.downloadToDrive = function (page) {
+        var filename = prompt("Enter File Name: ");
+        if (filename) {
+            socket.emit('saveToDrive', { data: page, name: filename });
+            $timeout(function () {
+                $scope.visitedPages[page.id].msg = "Uploading To Drive";
+            });
+        }
+    }
+    $scope.clearVisitedPages = function () {
+        $scope.visitedPages = {};
+        socket.emit('clearVisitedPages');
+    }
+    $scope.redirectToLoginUrl = function (url) {
+        window.location = url;
+    }
+    $scope.openUrl = function () {
+        window.open(window.location.origin + '/proxy/' + $scope.url);
+    }
+    $scope.setProgress = function (data) {
+        var obj = $scope.visitedPages[data.id];
+        if (!obj) {
+            $scope.visitedPages[data.id] = data;
+            obj = data;
+        }
+        if (data.progress) {
+            $timeout(function () {
+                $scope.visitedPages[data.id].progress = data.progress;
+                var progressBar = $('#progress-' + data.id);
+                var percent = data.progress;
+                var percentNow = progressBar.progress('get percent');
+                if (percent >= percentNow) {
+                    progressBar.progress({ percent: percent });
+                }
+            });
+        }
+
     }
 });
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 //Requires
-const http = require('http');
 const google = require('googleapis');
 const FILE = require('fs');
-const io = require('socket.io');
 const url = require('url');
-var httpProxy = require('http-proxy');
+const Unblocker = require('unblocker');
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const shortid = require('shortid');
+const mime = require('mime');
+const session = require('express-session');
 
 //Constants
 const PORT = Number(process.env.PORT || 3000);
-//const PARENT_HOST = '127.0.0.1:3000';
-const PARENT_HOST = 'directtodrive.herokuapp.com';
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const REDIRECT_URL = 'https://directtodrive.herokuapp.com/oauthCallback';
@@ -20,11 +22,17 @@ const SCOPES = [
 
 //Init
 const OAuth2 = google.auth.OAuth2;
-const oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
-google.options({ auth: oauth2Client }); // set auth as a global default
-var proxy = httpProxy.createProxyServer({}); // init http-proxy
+var oauth2ClientArray = {};
+var capture = false;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+var visitedPages = {};
 
-function getConsentPageURL() {
+function newOauthClient() {
+    return new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+}
+function getConsentPageURL(oauth2Client) {
     var url = oauth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES
@@ -32,83 +40,108 @@ function getConsentPageURL() {
     return url;
 }
 
-function handleOauthCallBack(url) {
-    var processed = url.split("?").split('&');
-    var params = {};
-    processed.forEach((data) => {
-        //data is like aaaa=bbbb
-        var t = data.split('=');
-        params[t[0]] = t[1];
-    });
-    var code = params.code;
-    oauth2Client.getToken(code, function (err, tokens) {
-        // Now tokens contains an access_token and an optional refresh_token. Save them.
-        if (!err) {
-            oauth2Client.setCredentials(tokens);
-        }
-    });
-}
-
-function uploadToDrive(localFile, mime, fileName) {
-    var drive = google.drive({ version: 'v3' });
-    drive.files.create({
+function uploadToDrive(stream, mime, fileName, oauth2Client, callback) {
+    var drive = google.drive({ version: 'v3', auth: oauth2Client });
+    return drive.files.create({
         resource: {
             name: fileName,
             mimeType: mime
         },
         media: {
             mimeType: mime,
-            body: FILE.createReadStream(localFile)
+            body: stream
         }
     }, callback);
 }
-function give404(response) {
-    response.writeHead(404);
-    response.end('<h1>404 Not Found</h1>');
-}
-function handler(request, response) {
-    var requested_path = request.url;  //like /someUrl
-    var host = request.headers.host;  //like host.com
-    if (host.includes(PARENT_HOST)) {
-        //running at server
-        if (requested_path == '/') {
-            //serve with google login page
-            FILE.readFile("index.html", (err, data) => {
-                response.writeHead(200);
-                response.end(data);
-            });
-        }
-        else if (requested_path.startsWith('/libs') || requested_path.startsWith('/js')) {
-            FILE.readFile(requested_path.substr(1), (err, data) => {
-                if (!err) {
-                    response.writeHead(200);
-                    response.end(data);
-                } else {
-                    give404(response);
-                }
-            });
-        }
-        else if (requested_path.startsWith('/oauthCallback')) {
-            //handle oauth Callback
-            handleOauthCallBack(requested_path);
-        }
-        else {//404
-            give404(response);
-        }
-    } else {
-        //is being used as proxy
-        listener.emit('pageVisited', { url: request.url });
-        proxy.web(request, response, { target: "http://"+request.headers.host },(e)=>{
-            listener.emit('pageVisited',{url:e});
+//TODO send pageVisited to its respective user using sessionID
+function middleware(data) {
+    var startedDownload = false;
+    var uniqid = shortid.generate();
+    var sessionID = data.clientRequest.sessionID;
+    var newFileName = null;
+    if (!data.contentType.startsWith('text/') && !data.contentType.startsWith('image/')) {
+        startedDownload = true;
+        var totalLength = data.headers['content-length'];
+        var downloadedLength = 0;
+        newFileName = uniqid + '.' + mime.extension(data.contentType);
+        FILE.closeSync(FILE.openSync('files/' + newFileName, 'w'));
+        var stream = FILE.createWriteStream(__dirname + '/files/' + newFileName);
+        data.stream.pipe(stream);
+        data.stream.on('data', (chunk) => {
+            downloadedLength += chunk.length;
+            progress = Math.round(((downloadedLength / totalLength) * 1000)) / 10;
+            io.emit('progress', { id: uniqid, progress: progress });
+            if (visitedPages[uniqid]) {
+                visitedPages[uniqid].progress = progress;
+            }
         });
+        var obj = { url: data.url, startedDownload: startedDownload, id: uniqid, mime: data.contentType, size: data.headers['content-length'], path: '/files/' + newFileName };
+        io.emit('pageVisited', obj);
+        visitedPages[uniqid] = obj;
     }
 }
 
-var server = http.createServer(handler);
-server.listen(PORT);
-var listener = io.listen(server);
-listener.sockets.on('connection', function (socket) {
-    socket.on('get-consent-page-url', (data) => {
-        socket.emit('consent-page-url', { 'url': getConsentPageURL() });
+var sessionMiddleware = session({
+    secret: "XYeMBetaCloud",
+    resave: false,
+    saveUninitialized: true
+});
+app.use(sessionMiddleware);
+app.use(new Unblocker({ prefix: '/proxy/', responseMiddleware: [middleware] }));
+app.use('/libs', express.static('libs'));
+app.use('/js', express.static('js'));
+app.use('/files', express.static('files'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/oauthCallback', (req, res) => {
+    var sessionID = req.sessionID;
+    var oauth2Client = oauth2ClientArray[sessionID];
+    if (!oauth2Client) { res.send('Invalid Attempt[E01]'); return false; }
+    var code = req.query.code;
+    if (code) {
+        oauth2Client.getToken(code, function (err, tokens) {
+            if (!err) {
+                oauth2Client.setCredentials(tokens);
+                res.redirect('/');
+            } else {
+                console.log("Error: " + err);
+                res.end('Error Occured');
+            }
+        });
+    } else {
+        res.send('Invalid Attempt[E03]');
+    }
+});
+io.use(function (socket, next) {
+    sessionMiddleware(socket.conn.request, socket.conn.request.res, next);
+});
+
+io.on('connection', function (client) {
+    var sessionID = client.conn.request.sessionID;
+    if (!oauth2ClientArray[sessionID]) {    //init a new oauth client if not present
+        oauth2ClientArray[sessionID] = newOauthClient();
+    }
+    var consentPageUrl = getConsentPageURL(oauth2ClientArray[sessionID]);
+    client.emit('status', { url: consentPageUrl, logged: (Object.keys(oauth2ClientArray[sessionID].credentials).length > 0) });
+    Object.keys(visitedPages).forEach((id) => {
+        client.emit('progress', visitedPages[id]);
+    });
+    client.on('clearVisitedPages', () => {
+        visitedPages = {};
+    });
+    client.on('saveToDrive', (data) => {
+        var stream = FILE.createReadStream(__dirname + data.data.path);
+        uploadToDrive(stream, data.data.mime, data.name, oauth2ClientArray[sessionID], (err, resp) => {
+            if (err) {
+                console.log(err);
+            } else {
+                client.emit('driveUploadSuccess', { name: resp.name, id: data.data.id });
+                visitedPages[data.data.id].msg = "Uploaded " + resp.name + " to Drive";
+            }
+        });
     });
 });
+
+server.listen(PORT);
+
