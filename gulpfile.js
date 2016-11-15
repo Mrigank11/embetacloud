0b64fd2b34ca97500cf52760ebbb1ccabd2c2be8
Mrigank11 <mrigankkrishan@gmail.com>
2016-11-16 00:37:00 +0530

    added postinstall for heroku

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var gulp = require('gulp-help')(require('gulp'));
+var gulp = require('gulp');
 var semanticBuild = require('./static/libs/semantic/tasks/build');
 var semanticClean = require('./static/libs/semantic/tasks/clean');
 
-gulp.task('build-semantic-ui', 'Build Semantic UI files', semanticBuild);
-gulp.task('clean-semantic-ui', 'Clean Semantic UI files', semanticClean);
+gulp.task('build-semantic-ui', semanticBuild);
+gulp.task('clean-semantic-ui', semanticClean);
 gulp.task('default', false, ['clean-semantic-ui', 'build-semantic-ui']);
