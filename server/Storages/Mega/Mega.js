cd1391cc303053cc2313fe3cc25ed22ab85d631a
Mrigank11 <mrigankkrishan@gmail.com>
2017-04-05 18:21:31 +0530

    full MEGA.co.nz support, closes #13

--- server/Storages/Mega/Mega.js
+++ server/Storages/Mega/Mega.js
@@ -5,12 +5,17 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var mega = require("mega");
+var FILE = require("fs-extra");
+var path = require("path");
+var mime = require("mime");
 var debug = require("debug")("eMCloud::Mega");
 var events_1 = require("events");
 var Mega = (function (_super) {
     __extends(Mega, _super);
     function Mega(credentials) {
         var _this = _super.call(this) || this;
+        _this.stack = [];
+        _this.stackProcessing = false;
         _this.creds = credentials;
         return _this;
         //store credentials, they can be username/password or OAuth Tokens etc.
@@ -25,29 +30,36 @@ var Mega = (function (_super) {
         //if authentication fails, call the callback as: callback(0)
         // when user requests a file upload, credentials from session will be used to initialize this class (the constructor will be called)
         var storage = mega({ email: query.username, password: query.password, keepalive: false }, function (err) {
-            if (err)
+            if (err) {
+                debug("Error: " + err);
                 callback(0);
+            }
         });
         storage.on('ready', function () {
             callback({ email: query.username, password: query.password });
         });
     };
-    Mega.prototype.uploadFile = function (readStream, totalSize, mime, filename) {
+    Mega.prototype.uploadFile = function (readStream, totalSize, mime, filename, parent, callback) {
         //handle the upload procedure
         //it should emit => progress        : {name,uploaded,size}
-        //                  fileUploaded    : {size, name , error} 
+        //                  fileUploaded    : {size, name , error}
+        debug('Uploading file %s ', filename);
         var self = this;
         var storage = mega({ email: this.creds.email, password: this.creds.password, keepalive: false }, function (err) {
             if (err) {
                 self.emit("fileUploaded", {
                     error: err
                 });
+                debug("Error: " + err);
             }
         });
-        var up = storage.upload({
+        var opts = {
             name: filename,
             size: totalSize
-        }, function (err, file) {
+        };
+        if (parent)
+            opts.target = parent;
+        var up = storage.upload(opts, function (err, file) {
             if (err) {
                 debug(err);
                 self.emit("fileUploaded", {
@@ -73,6 +85,82 @@ var Mega = (function (_super) {
                 name: filename,
                 error: false
             });
+            debug("Uploaded: %s", filename);
+            callback();
+        });
+    };
+    Mega.prototype.makeDir = function (name, callback, parent) {
+        debug("Creating Directory %s", name);
+        var self = this;
+        var storage = mega({ email: this.creds.email, password: this.creds.password, keepalive: false }, function (err) {
+            if (err) {
+                self.emit("fileUploaded", {
+                    error: err
+                });
+                debug("Error: " + err);
+            }
+        });
+        storage.mkdir({ name: name, target: parent }, function (err, r) {
+            callback(r);
+        });
+    };
+    Mega.prototype.uploadStack = function () {
+        var _this = this;
+        if (this.stack.length > 0) {
+            this.stackProcessing = true;
+            var params = this.stack[0];
+            this.uploadFile(params[0], params[1], params[2], params[3], params[4], function (err, resp) {
+                if (err) {
+                    debug("Error processing stack: " + err);
+                }
+                else {
+                    _this.stack.splice(0, 1);
+                    _this.uploadStack();
+                }
+            });
+        }
+        else {
+            this.stackProcessing = false;
+        }
+    };
+    Mega.prototype.uploadDir = function (folderPath, parent) {
+        var _this = this;
+        //upload a local directory
+        //should emit    => addSize    : size      size in bytes to be added to total upload size
+        //may emit       => mkdir      : name      name of cloud directory created
+        FILE.readdir(folderPath, function (err, list) {
+            if (!err) {
+                list.forEach(function (item) {
+                    FILE.lstat(path.join(folderPath, item), function (e, stat) {
+                        _this.emit("addSize", {
+                            size: stat.size
+                        });
+                        if (!err) {
+                            if (stat.isDirectory()) {
+                                _this.makeDir(item, function (newParentId) {
+                                    _this.uploadDir(path.join(folderPath, item), newParentId);
+                                }, parent);
+                            }
+                            else {
+                                var fullPath = path.join(folderPath, item);
+                                var stream = FILE.createReadStream(fullPath);
+                                //this.uploadFile(stream, stat.size, mime.lookup(fullPath), item, oauth2Client, parentId);
+                                _this.stack.push([stream, stat.size, mime.lookup(fullPath), item, parent]);
+                                if (!_this.stackProcessing) {
+                                    //stack not running
+                                    _this.uploadStack();
+                                }
+                            }
+                        }
+                        else {
+                            debug(err);
+                        }
+                    });
+                });
+            }
+            else {
+                debug(err);
+            }
         });
     };
     return Mega;
--- server/Storages/Mega/Mega.ts
+++ server/Storages/Mega/Mega.ts
@@ -1,10 +1,16 @@
 const mega = require("mega");
+const FILE = require("fs-extra");
+const path = require("path");
+const mime = require("mime");
 const debug = require("debug")("eMCloud::Mega");
 
 import { EventEmitter } from 'events';
 
 export class Mega extends EventEmitter {
     creds;
+    private stack: Array<Array<any>> = [];
+    private stackProcessing: boolean = false;
+
     constructor(credentials) {
         super();
         this.creds = credentials;
@@ -20,28 +26,36 @@ export class Mega extends EventEmitter {
         //if authentication fails, call the callback as: callback(0)
         // when user requests a file upload, credentials from session will be used to initialize this class (the constructor will be called)
         var storage = mega({ email: query.username, password: query.password, keepalive: false }, (err) => {
-            if (err) callback(0);
+            if (err) {
+                debug("Error: " + err);
+                callback(0);
+            }
         });
         storage.on('ready', () => {
             callback({ email: query.username, password: query.password });
         })
     }
-    public uploadFile(readStream, totalSize, mime, filename) {
+    public uploadFile(readStream, totalSize, mime, filename, parent?, callback?) {
         //handle the upload procedure
         //it should emit => progress        : {name,uploaded,size}
-        //                  fileUploaded    : {size, name , error} 
+        //                  fileUploaded    : {size, name , error}
+        debug('Uploading file %s ', filename);
         var self = this;
         var storage = mega({ email: this.creds.email, password: this.creds.password, keepalive: false }, (err) => {
             if (err) {
                 self.emit("fileUploaded", {
                     error: err
                 });
+                debug("Error: " + err);
             }
         });
-        var up = storage.upload({
+        var opts: any = {
             name: filename,
             size: totalSize
-        },
+        };
+        if (parent)
+            opts.target = parent;
+        var up = storage.upload(opts,
             function (err, file) {
                 if (err) {
                     debug(err);
@@ -67,12 +81,77 @@ export class Mega extends EventEmitter {
                 size: totalSize,
                 name: filename,
                 error: false
-            })
+            });
+            debug("Uploaded: %s", filename);
+            callback();
         })
     }
-    //public uploadDir(localFolderPath) {          //not necessary
-    //upload a local directory
-    //should emit    => addSize    : size      size in bytes to be added to total upload size
-    //may emit       => mkdir      : name      name of cloud directory created
-    //}
+
+    private makeDir(name, callback, parent?) {
+        debug("Creating Directory %s", name);
+        var self = this;
+        var storage = mega({ email: this.creds.email, password: this.creds.password, keepalive: false }, (err) => {
+            if (err) {
+                self.emit("fileUploaded", {
+                    error: err
+                });
+                debug("Error: " + err);
+            }
+        });
+        storage.mkdir({ name: name, target: parent }, function (err, r) {
+            callback(r);
+        })
+    }
+    private uploadStack() {
+        if (this.stack.length > 0) {
+            this.stackProcessing = true;
+            var params = this.stack[0];
+            this.uploadFile(params[0], params[1], params[2], params[3], params[4], (err, resp) => {
+                if (err) {
+                    debug("Error processing stack: " + err);
+                } else {
+                    this.stack.splice(0, 1);
+                    this.uploadStack();
+                }
+            });
+        } else {
+            this.stackProcessing = false;
+        }
+    }
+    public uploadDir(folderPath, parent?) {          //not necessary
+        //upload a local directory
+        //should emit    => addSize    : size      size in bytes to be added to total upload size
+        //may emit       => mkdir      : name      name of cloud directory created
+        FILE.readdir(folderPath, (err, list) => {
+            if (!err) {
+                list.forEach((item) => {
+                    FILE.lstat(path.join(folderPath, item), (e, stat) => {
+                        this.emit("addSize", {
+                            size: stat.size
+                        });
+                        if (!err) {
+                            if (stat.isDirectory()) {
+                                this.makeDir(item, (newParentId) => {
+                                    this.uploadDir(path.join(folderPath, item), newParentId);
+                                }, parent);
+                            } else {
+                                var fullPath = path.join(folderPath, item);
+                                var stream = FILE.createReadStream(fullPath);
+                                //this.uploadFile(stream, stat.size, mime.lookup(fullPath), item, oauth2Client, parentId);
+                                this.stack.push([stream, stat.size, mime.lookup(fullPath), item, parent]);
+                                if (!this.stackProcessing) {
+                                    //stack not running
+                                    this.uploadStack();
+                                }
+                            }
+                        } else {
+                            debug(err);
+                        }
+                    });
+                });
+            } else {
+                debug(err);
+            }
+        });
+    }
 }
